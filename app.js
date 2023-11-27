997642405d5a2a785170a80955cbf2518701a376
ThomasNg2 <ThNguyen2802@gmail.com>
2023-11-27 11:27:38 +0100

    renamed sw

--- app.js
+++ app.js
@@ -589,6 +589,6 @@ document.addEventListener("DOMContentLoaded", function (_e) {
     }
 
     if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("sw.js");
+        navigator.serviceWorker.register("./service-worker.js");
     }
 });
